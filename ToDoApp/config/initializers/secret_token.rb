99e6f41fac7c38c9f0f9a9e90c1321ac1fccf72a
App Academy Student <student@appacademy.io>
2013-04-05 09:27:11 -0700

    initial commit

---
+++ ToDoApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDoApp::Application.config.secret_token = '1575116b414601e25e491cdede288d07e2778afa7b9b6a80ecda817ca907bded090e2c5a308f6e7caff6cf944283904c29b25a2ccd14ff21051470b8dc785403'
